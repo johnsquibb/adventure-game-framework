969a3d05dbd045119e73c9492980f8541d290f7e
John Squibb <git@johnsquibb.com>
2021-05-08 13:01:22 +0000

    Adding container item tests

--- test/Item/ContainerItemTest.php
+++ test/Item/ContainerItemTest.php
@@ -2,7 +2,190 @@
 
 namespace AdventureGame\Test\Item;
 
-class ContainerItemTest extends ContainerTest
-{
+use AdventureGame\Item\Container;
+use AdventureGame\Item\ContainerItem;
+use AdventureGame\Item\Item;
+use PHPUnit\Framework\TestCase;
 
+class ContainerItemTest extends TestCase
+{
+    public function testCreateItem()
+    {
+        $id = 'test-item';
+        $name = 'Test Item';
+        $description = 'Test Item Description';
+        $tag = 'test';
+        $item = new ContainerItem($id, $name, $description, $tag);
+
+        $this->assertEquals($id, $item->id);
+        $this->assertEquals($name, $item->name);
+        $this->assertEquals($description, $item->description);
+        $this->assertEquals($tag, $item->tag);
+    }
+
+    public function testAddContainerItemToContainer()
+    {
+        $container = new Container();
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+    }
+
+    public function testAddContainerItemToContainerItem()
+    {
+        $container = new ContainerItem(
+            'test-containerItem',
+            'Test ContainerItem',
+            'Test ContainerItem Description',
+            'test-container'
+        );
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+    }
+
+    public function testGetContainerItemFromContainer()
+    {
+        $container = new Container();
+        $this->assertNull($container->getItemById('nothing'));
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+    }
+
+    public function testGetContainerItemFromContainerIem()
+    {
+        $container = new ContainerItem(
+            'test-containerItem',
+            'Test ContainerItem',
+            'Test ContainerItem Description',
+            'test-container'
+        );
+
+        $this->assertNull($container->getItemById('nothing'));
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+    }
+
+    public function testGetContainerItemsFromContainerByTag()
+    {
+        $container = new Container();
+        $this->assertEmpty($container->getItemsByTag('test'));
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+
+        $this->assertEquals([$item], $container->getItemsByTag($item->tag));
+
+        $item2 = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+
+        $this->assertEquals([$item, $item2], $container->getItemsByTag($item->tag));
+    }
+
+    public function testGetContainerItemsFromContainerItemByTag()
+    {
+        $container = new ContainerItem(
+            'test-containerItem',
+            'Test ContainerItem',
+            'Test ContainerItem Description',
+            'test-container'
+        );
+
+        $this->assertEmpty($container->getItemsByTag('test'));
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+
+        $this->assertEquals([$item], $container->getItemsByTag($item->tag));
+
+        $item2 = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+        $container->addItem($item);
+
+        $this->assertEquals([$item, $item2], $container->getItemsByTag($item->tag));
+    }
+
+    public function testRemoveContainerItemFromContainer()
+    {
+        $container = new Container();
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+
+        $container->removeItemById($item->id);
+        $this->assertNull($container->getItemById($item->id));
+    }
+
+    public function testRemoveContainerItemFromContainerItem()
+    {
+        $container = new ContainerItem(
+            'test-containerItem',
+            'Test ContainerItem',
+            'Test ContainerItem Description',
+            'test-container'
+        );
+
+        $item = new ContainerItem(
+            'test-item',
+            'Test Item',
+            'Test Item Description',
+            'test'
+        );
+
+        $container->addItem($item);
+        $this->assertEquals($item, $container->getItemById($item->id));
+
+        $container->removeItemById($item->id);
+        $this->assertNull($container->getItemById($item->id));
+    }
 }
