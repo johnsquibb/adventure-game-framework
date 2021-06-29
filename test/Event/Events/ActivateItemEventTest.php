7bfe2c23ced126abae62afe7b3a6068318551835
John Squibb <git@johnsquibb.com>
2021-06-29 13:44:28 +0000

    Adding test for activate item trigger and event

--- test/Event/Events/ActivateItemEventTest.php
+++ test/Event/Events/ActivateItemEventTest.php
@@ -2,8 +2,13 @@
 
 namespace AdventureGame\Test\Event\Events;
 
+use AdventureGame\Event\AbstractInventoryEvent;
 use AdventureGame\Event\Events\ActivateItemEvent;
+use AdventureGame\Event\Triggers\AddItemToLocationUseTrigger;
+use AdventureGame\Event\Triggers\RemoveItemFromLocationUseTrigger;
+use AdventureGame\Item\AbstractItem;
 use AdventureGame\Item\Container;
+use AdventureGame\Item\Item;
 use AdventureGame\Location\Location;
 use AdventureGame\Response\Response;
 use AdventureGame\Test\Event\AbstractEventTest;
@@ -41,5 +46,77 @@ class ActivateItemEventTest extends AbstractEventTest
                 'test-item-id'
             )
         );
+    }
+
+    public function testActivateItemEventAddItemToLocationEvent()
+    {
+        $location = new Location(
+            'test-location-id',
+            'Test Location',
+            ['A test location'],
+            new Container(),
+            []
+        );
+
+        $item = new Item('test-item-id', 'test item', [], []);
+        $trigger = new AddItemToLocationUseTrigger($item, 1);
+
+        $event = new ActivateItemEvent(
+            $trigger,
+            'test-item-id',
+            'test-location-id',
+        );
+
+        $gameController = $this->createGameController();
+        $gameController->mapController->addLocation($location);
+        $gameController->mapController->setPlayerLocationById('test-location-id');
+        $gameController->eventController->addEvent($event);
+
+        $response = $gameController->eventController->processActivateItemEvents(
+            $gameController,
+            'test-item-id'
+        );
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals('Revealed "test item".', $response->getMessages()[0]);
+    }
+
+    public function testActivateItemEventRemoveItemFromLocationEvent()
+    {
+        $container = new Container();
+        $item = new Item('test-item-id', 'test item', [], []);
+        $container->addItem($item);
+
+        $location = new Location(
+            'test-location-id',
+            'Test Location',
+            ['A test location'],
+            $container,
+            []
+        );
+
+        $trigger = new RemoveItemFromLocationUseTrigger($item, 1);
+
+        $event = new ActivateItemEvent(
+            $trigger,
+            'test-item-id',
+            'test-location-id',
+        );
+
+        $gameController = $this->createGameController();
+        $gameController->mapController->addLocation($location);
+        $gameController->mapController->setPlayerLocationById('test-location-id');
+        $gameController->eventController->addEvent($event);
+
+        $this->assertEquals([$item], $container->getItems());
+
+        $response = $gameController->eventController->processActivateItemEvents(
+            $gameController,
+            'test-item-id'
+        );
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals('Removed "test item".', $response->getMessages()[0]);
+        $this->assertEquals([], $container->getItems());
     }
 }
